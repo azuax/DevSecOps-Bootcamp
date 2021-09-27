d2b95455f247b46319ea38a381e60ff1f0175c47
azuax <azuaxx@gmail.com>
2021-09-27 18:25:41 -0300

    Added security issues to test

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask
+from flask import request
+import subprocess
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/ping")
+def ping():
+    ip = request.args.get('ip')
+    result = subprocess.check_output(["ping", "-c 2", "www.google.com"])
+    return result
+
+@app.route("/very-secret")
+def very_secret():
+    secret_key = bcc286bbbe4353e6a97ae169729ed4a5
+    return """
+    The key is: %s
+    """.format(secret_key)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0') 
