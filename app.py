dab9bf487a389c3870d21e7f2f9f1aa5c5871ebf
azuax <azuaxx@gmail.com>
2021-09-28 16:40:08 -0300

    Hotfix: variable

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask
 from flask import request
 import subprocess
+from secrets_manager import get_secret
 
 app = Flask(__name__)
+
+api_key = get_secret()
+
 
 @app.route("/")
 def hello():
@@ -11,15 +15,19 @@ def hello():
 @app.route("/ping")
 def ping():
     ip = request.args.get('ip')
-    result = subprocess.check_output(["ping", "-c 2", "www.google.com"])
+    result = subprocess.check_output(["ping", "-c 2", ip])
     return result
 
 @app.route("/very-secret")
 def very_secret():
     secret_key = "bcc286bbbe4353e6a97ae169729ed4a5"
-    return """
-    The key is: %s
-    """.format(secret_key)
+    return f"""
+    The key is: {secret_key}
+    """
+
+@app.route("/best-secret")
+def best_secret():
+    return api_key
 
 
 if __name__ == '__main__':
